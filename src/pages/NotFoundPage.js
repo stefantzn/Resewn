848e7d1b094f3b8058774ac19612ce11ee6d7cfc
he-is-harry <harryhe_@hotmail.com>
2024-03-15 18:05:35 -0400

    Added routing

---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,10 @@
+function NotFoundPage() {
+  return (
+    <div >
+      <h1>404: Page Not Found</h1>
+      <p>It looks like this page doesn't exit, whoops!</p>
+    </div>
+  );
+}
+  
+export default NotFoundPage;
