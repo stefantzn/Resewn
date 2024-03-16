e8dab916a894e01e607dddfef8bc3fca047fae35
Stefan Tuczynski <tuczynskis@gmail.com>
2024-03-16 11:33:48 -0400

    ewew

---
+++ src/components/Shop.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Marketplace from "./MarketplacePage"
+
+const Shop = () => {
+  return (
+    <div>
+        < Marketplace/>
+    </div>
+  );
+};
+
+export default Shop;
